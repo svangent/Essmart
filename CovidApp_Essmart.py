a68cf61b751d8c40b136dcadd5bf73276f9757ca
hazeyblu <48317480+hazeyblu@users.noreply.github.com>
2021-05-31 10:59:33 +0530

    Update CovidApp_Essmart.py

--- CovidApp_Essmart.py
+++ CovidApp_Essmart.py
@@ -238,7 +238,7 @@ def analysis_layout(district_shorter_data, district_list):
         c3.markdown("<h3 style='text-align: center; color: #39A275;'>Current Condition</h3>",
                     unsafe_allow_html=True)
 
-        if significance_count <= 1.5:
+        if significance_count < 1.5:
             c3.markdown("<h1 style='text-align: center;'>🔴</h1>",
                         unsafe_allow_html=True)
         elif significance_count <= 2.1:
