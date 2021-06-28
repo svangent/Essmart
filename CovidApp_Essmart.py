6c96768ea71bdd8f820a7ef04591dc1d4a6febfa
hazeyblu <48317480+hazeyblu@users.noreply.github.com>
2021-06-28 10:46:33 +0530

    Update CovidApp_Essmart.py

--- CovidApp_Essmart.py
+++ CovidApp_Essmart.py
@@ -181,7 +181,7 @@ def analysis_layout(district_shorter_data, district_list):
         district_detailed = c1.radio(label="Deep Dive", options=district_list, index=0,
                                      help="Select district (refer to branch details above) to see historical details")
         windows = [7, 14, 21, 28]
-        columns = []
+        columns = ['NewCases']
         c1.markdown("<h4 style='text-align: center; color: #39A275;'>Average New Cases for Graph</h4>",
                     unsafe_allow_html=True)
         for window in windows:
@@ -227,7 +227,7 @@ def analysis_layout(district_shorter_data, district_list):
         if d7 < 1000:
             significance_count += 1
             new_case += 1
-        if d1 == 0:
+        if d1 < 100:
             significance_count += 1
             new_case += 1
 
